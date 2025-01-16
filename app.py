ca77312a408889a86b3f1c1d18f7571c1055b336
Mariela Badillo <mlbadillo8@gmail.com>
2025-01-15 19:49:08 -0500

    implement and style home page

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/startChat")
+def startChat():
+    return render_template("chat.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
